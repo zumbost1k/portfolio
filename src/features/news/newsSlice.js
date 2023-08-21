9f7699136bc708d21e5efdb81acd3b57762818f4
zumbost1k <matusevich.mi@gmail.com>
2023-08-21 18:03:39 +0300

    add news section

--- src/features/news/newsSlice.js
+++ src/features/news/newsSlice.js
@@ -1,9 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { v4 } from 'uuid'
 
 const initialState = {
     allNews: [
         {
-            newsTitle: 'Hello World',
+            newsId:v4(),
+            newsTitle: 'Hello World!',
             newsDate: '18 Aug 2023',
             newsText: 'This is my site where I try and post about what I`m up to and how to do things with code.You can follow along by hitting the "Follow" button up above.Or if you`re only interested in stuff I wrote about, check out /content.',
             newsPhotoPath: 'flower.jpg',
--- src/news/news.jsx
+++ src/news/news.jsx
@@ -9,11 +9,13 @@ const News = () => {
         <section className="news_section">
             {allNews.map(news => {
                 return (
-                    <div className="news_container">
+                    <div key={news.newsId} className="news_container">
                         <img className="news_avatar" src="/photos/me.jpg" alt="my avatar" width='40' height='40' />
                         <div className="news_content_container">
                             <div className="news_author_name_date"><p>Misha • </p> <time className="news_date" dateTime={news.newsDate}>{news.newsDate}</time></div>
-                            <h3>{news.newsTitle}</h3>
+                            <h3 className="news_title">{news.newsTitle}</h3>
+                            <p className="news_text">{news.newsText}</p>
+                            <img width='600' height='600' className="news_photo" src={`/photos/newsPhotos/${news.newsPhotoPath}`} alt={news.newsTitle} />
                         </div>
                     </div>
                 )
