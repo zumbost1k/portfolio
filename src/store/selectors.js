4545c6a66b19f9a7e6e5617d8fa7fda54b91f0ff
zumbost1k <matusevich.mi@gmail.com>
2024-02-06 19:41:02 +0300

    remove slice project

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,2 +1,1 @@
 export const allNewsList = state => state.allNews.allNews
-export const allProjectsList = state => state.allProjects.allProjects
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { AllNewsSlice } from '@/features/news/newsSlice';
-import { AllProjectsSlice } from '@/features/allProjects/allProjectsSlice';
 
 export const store = configureStore({
     reducer: {
         allNews: AllNewsSlice.reducer,
-        allProjects: AllProjectsSlice.reducer
     }
 })
